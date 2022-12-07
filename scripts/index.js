b0eb8529776e9121083eedede34d8015d511a8b2
Amy_Robrigado <ARobrigado@nyc.yearup.org>
2022-12-07 11:57:25 -0500

    Modified pages

--- scripts/index.js
+++ scripts/index.js
@@ -12,5 +12,11 @@ function pickByCityDropdown(){
     citySelectOpt.textContent = "Select a City...";
     citySelect.appendChild(citySelectOpt);
 
+    let cityLength = cities.length; 
+    for(let i = 0; i < cityLength; i++){
+        let opt1 = document.createElement("option");
+        opt1.textContent = cities[i].city;
 
+        citySelect.appendChild(opt1);
+    }
 }

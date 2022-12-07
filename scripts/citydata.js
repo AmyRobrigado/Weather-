2f33b5ffa3e5cd2aa6d5d885cdcb258e29a6ac9d
Amy_Robrigado <ARobrigado@nyc.yearup.org>
2022-12-07 11:17:30 -0500

    Modified js and html files

---
+++ scripts/citydata.js
@@ -0,0 +1,27 @@
+let cities = [
+    {  
+        city: "El Paso, TX",
+        latitude: 31.7619, 
+        longitude: -106.4850,
+    },
+    {
+        city: "Buffalo, NY",
+        latitude: 42.8864,
+        longitude: -78.8784,
+    },
+    {
+        city: "Everett, WA",
+        latitude: 47.9790, 
+        longitude: -122.2021,        
+    },
+    {
+        city: "Salt Lake City, UT",
+        latitude: 40.7608,
+        longitude: -111.8910, 
+    },
+    {
+        city: "Portland, OR", 
+        latitude: 45.5152,
+        longitude: -122.6784,
+    },
+]
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,16 @@
 "use strict"
 
 window.onload = function(){
-    
+    pickByCityDropdown();
+}
+
+function pickByCityDropdown(){
+    const citySelect = document.getElementById("citySlct");
+    let citySelectOpt = document.createElement("option"); 
+
+    citySelectOpt.value = "";
+    citySelectOpt.textContent = "Select a City...";
+    citySelect.appendChild(citySelectOpt);
+
+
 }

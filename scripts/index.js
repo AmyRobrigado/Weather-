c4317209b47f63d48aa823f64e0313ff33c04aa8
Amy_Robrigado <ARobrigado@nyc.yearup.org>
2022-12-09 12:36:18 -0500

    Completed

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 "use strict"
 
 window.onload = function(){
-    pickByCityDropdown();
+    document.getElementById("citySlct").onchange = currentlySelectedCity;
 
-    
+    pickByCityDropdown();
 }
 
 function pickByCityDropdown(){
@@ -23,3 +23,46 @@ function pickByCityDropdown(){
     }
 }
 
+function currentlySelectedCity(){
+    let selectYourCity = document.getElementById("citySlct").value
+    let tbody = document.getElementById("tbody");
+
+    tbody.innerHTML = "";
+
+    for(let i = 0; i < cities.length; i++){
+        let stationLookupUrl =`https://api.weather.gov/points/${cities[i].latitude},${cities[i].longitude}`;
+        if (selectYourCity == cities[i].city)
+        fetch(stationLookupUrl)
+        .then(response => response.json())
+        .then(data => {
+
+        let weatherUrl = data.properties.forecast;
+
+        fetch(weatherUrl)
+        .then(response => response.json())
+        .then(data => {
+        for(let w = 0; w < data.properties.periods.length; w++){
+
+            let tr = document.createElement("tr");
+            tbody.appendChild(tr);
+
+            //-----------------------------------------------------
+
+            let day = document.createElement("td");
+            day.innerHTML = data.properties.periods[w].name;
+            tr.appendChild(day);
+
+            let temperature = document.createElement("td");
+            temperature.innerHTML = data.properties.periods[w].temperature + " " + data.properties.periods[w].temperatureUnit
+            tr.appendChild(temperature);
+
+            let forecast = document.createElement("td");
+            forecast.innerHTML = data.properties.periods[w].detailedForecast
+            tr.appendChild(forecast);
+        
+        }
+        
+        })
+        })
+    }
+}
